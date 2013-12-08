ed6d5425db23ce045743b0824857bd59c1427902
Matvey <alekseev-matvey@mail.ru>
2013-12-08 23:01:48 +0400

    Finish ! What?

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,14 +10,14 @@ describe "StaticPages" do
 			end
 			it "should have the right title" do
 				visit '/static_pages/home'
-				page.should have_selector('title', :text => 'Home')
+				page.should have_selector('title', :text=> 'Home')
 			end
 	end
 	describe "Help page" do
   	
   		it "should have the content 'Help!" do
   			visit '/static_pages/help'
-  			page.should have_selector('h1', :text => 'Help')
+  			page.should have_selector('h1', :text=> 'Help')
     	end
     	it "should have the right title" do
 				visit '/static_pages/help'
@@ -32,7 +32,7 @@ describe "StaticPages" do
     	end
     	it "should have the right title" do
 				visit '/static_pages/about'
-				page.should have_selector('title', :text => ' About Us')
+				page.should have_selector('title', :text=> ' About Us')
 		end
 	end
 end
